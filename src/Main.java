a6ce49fa3489761658f427944a01be90e4f1329c
İmranov <semtexpert@gmail.com>
2023-12-29 16:36:14 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static boolean isPalindrom(int number) {
+        int temp = number;
+        int reverseNumber = 0;
+        int lastNumber;
+        while (temp != 0) {
+            System.out.println("==============");
+            System.out.println("Number :" + temp);
+            lastNumber = temp % 10;
+            System.out.println("Son basamak: " + lastNumber);
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            System.out.println("yeni Sayı; " + reverseNumber);
+            temp /= 10;
+        }
+        if(number==reverseNumber)
+        {
+            System.out.println("Girdiğiniz Sayı Bir Palindrom Sayıdır.");
+        }
+        else
+        {
+            System.out.println("Girdiğiniz Sayı Bir Palindrom Sayı Değildir.");
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("Girmek İstediğiniz Sayı: ");
+        number= input.nextInt();
+        isPalindrom(number);
+
     }
 }
